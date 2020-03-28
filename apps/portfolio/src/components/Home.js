ecb3d0a05b6df6f837a367fe20f10a36a49a7082
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-03-27 21:18:46 -0500

    style(portfolio): 💄 printWidth 80

--- apps/portfolio/src/components/Home.js
+++ apps/portfolio/src/components/Home.js
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types'
 
 const Home = ({ isEnglish }) => (
   <div className='title'>
-    <h1 className='title__name'>{isEnglish ? "Hi, I'm Jimmy" : 'Hola, Soy Jimmy'}</h1>
-    <h2 className='title__caption'>{isEnglish ? "I'm a Web Developer" : 'Soy un Desarrollador de Web'}</h2>
+    <h1 className='title__name'>
+      {isEnglish ? "Hi, I'm Jimmy" : 'Hola, Soy Jimmy'}
+    </h1>
+    <h2 className='title__caption'>
+      {isEnglish ? "I'm a Web Developer" : 'Soy un Desarrollador de Web'}
+    </h2>
   </div>
 )
 
