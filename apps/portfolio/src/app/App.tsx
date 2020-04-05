e5f13d4e8c7fca06d491aa0dbc4ed85f8158f97c
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-05 09:15:05 -0500

    refactor(portfolio): 💡 seperated Router from App

--- apps/portfolio/src/app/App.tsx
+++ apps/portfolio/src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { useDynamicHeight } from '@jimmy-guzman/hooks'
 
 import Nav from '../components/Nav'
@@ -15,28 +15,26 @@ export const App = () => {
   const appHeight = useDynamicHeight()
 
   return (
-    <BrowserRouter>
-      <div className='wrapper' style={{ height: `${appHeight}px` }}>
-        <Options updateLang={updateLang} isEnglish={isEnglish} />
-        <Switch>
-          <Route exact path='/' render={() => <Home isEnglish={isEnglish} />} />
-          <Route path='/about' render={() => <About isEnglish={isEnglish} />} />
-          <Route
-            path='/projects/'
-            render={() => <Projects isEnglish={isEnglish} />}
-          />
-          <Route
-            path='/contact'
-            render={() => <Contact isEnglish={isEnglish} />}
-          />
-          <Route
-            render={() => (
-              <h2>404 Error: URL does not match any existing paths!</h2>
-            )}
-          />
-        </Switch>
-        <Nav isEnglish={isEnglish} />
-      </div>
-    </BrowserRouter>
+    <div className='wrapper' style={{ height: `${appHeight}px` }}>
+      <Options updateLang={updateLang} isEnglish={isEnglish} />
+      <Switch>
+        <Route exact path='/' render={() => <Home isEnglish={isEnglish} />} />
+        <Route path='/about' render={() => <About isEnglish={isEnglish} />} />
+        <Route
+          path='/projects/'
+          render={() => <Projects isEnglish={isEnglish} />}
+        />
+        <Route
+          path='/contact'
+          render={() => <Contact isEnglish={isEnglish} />}
+        />
+        <Route
+          render={() => (
+            <h2>404 Error: URL does not match any existing paths!</h2>
+          )}
+        />
+      </Switch>
+      <Nav isEnglish={isEnglish} />
+    </div>
   )
 }
