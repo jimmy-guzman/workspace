e760d13e621ce10f4b4f9ccc533184bb345a2d15
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-05 09:15:05 -0500

    refactor(portfolio): 💡 seperated Router from App

--- apps/portfolio/src/index.js
+++ apps/portfolio/src/index.js
@@ -2,9 +2,10 @@ import 'normalize.css'
 import './scss/index.scss'
 import React from 'react'
 import { render } from 'react-dom'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import WebFont from 'webfontloader'
-import { App } from './app'
+
+import { App } from './App'
 
 WebFont.load({
   google: {
@@ -12,4 +13,9 @@ WebFont.load({
   }
 })
 
-render(<App />, document.getElementById('root'))
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
