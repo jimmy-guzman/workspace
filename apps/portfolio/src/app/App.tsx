ec2242ac6a2e5b53aa0b8e7926ef69fe840f02ae
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-04 21:37:53 -0500

    refactor(portfolio): 💡 changed to typescript and hooks
    ✅ Closes: #34

---
+++ apps/portfolio/src/app/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { useDynamicHeight } from '@jimmy-guzman/hooks'
+
+import Nav from '../components/Nav'
+import Options from '../components/Options'
+import Home from '../components/Home'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import { useLanguage } from '../hooks'
+
+export const App = () => {
+  const [isEnglish, updateLang] = useLanguage()
+  const appHeight = useDynamicHeight()
+
+  return (
+    <BrowserRouter>
+      <div className='wrapper' style={{ height: `${appHeight}px` }}>
+        <Options updateLang={updateLang} isEnglish={isEnglish} />
+        <Switch>
+          <Route exact path='/' render={() => <Home isEnglish={isEnglish} />} />
+          <Route path='/about' render={() => <About isEnglish={isEnglish} />} />
+          <Route
+            path='/projects/'
+            render={() => <Projects isEnglish={isEnglish} />}
+          />
+          <Route
+            path='/contact'
+            render={() => <Contact isEnglish={isEnglish} />}
+          />
+          <Route
+            render={() => (
+              <h2>404 Error: URL does not match any existing paths!</h2>
+            )}
+          />
+        </Switch>
+        <Nav isEnglish={isEnglish} />
+      </div>
+    </BrowserRouter>
+  )
+}
