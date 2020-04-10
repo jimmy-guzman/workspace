166e9bf14f383d183c6caa37b9dccff4a428b91c
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-10 12:01:28 -0500

    refactor(portfolio): 💡 convert to ProgressBar to typescript

---
+++ apps/portfolio/src/components/ProgressBar/ProgressBar.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { ProgressBar } from './ProgressBar'
+
+export default { title: 'apps/portfolio/ProgressBar', component: ProgressBar }
+
+export const normal = () => <ProgressBar />
