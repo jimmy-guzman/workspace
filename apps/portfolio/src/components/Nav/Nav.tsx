b927d93363746910e9c0a751a4bb2781ac4dd32a
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-10 16:32:31 -0500

    refactor(portfolio): 💡 encapsulated Nav styles within itself

--- apps/portfolio/src/components/Nav/Nav.tsx
+++ apps/portfolio/src/components/Nav/Nav.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import './Nav.scss'
+
+export interface Links {
+  link: string
+  name: string
+  spanish: string
+}
 export interface NavProps {
   isEnglish?: boolean
+  links: Links[]
 }
+export const defaultLinks: Links[] = [
+  { link: '/about', name: 'About', spanish: 'Sobre Mi' },
+  { link: '/projects', name: 'Projects', spanish: 'Proyectos' },
+  { link: '/contact', name: 'Contact', spanish: 'Contacto' }
+]
 
-export const Nav = ({ isEnglish = true }: NavProps) => (
+export const Nav = ({ isEnglish = true, links = defaultLinks }: NavProps) => (
   <nav className='main-nav'>
-    <NavLink to='/about'>{isEnglish ? 'About' : 'Sobre Mi'}</NavLink>
-    <NavLink to='/projects'>{isEnglish ? 'Projects' : 'Proyectos'}</NavLink>
-    <NavLink to='/contact'>{isEnglish ? 'Contact' : 'Contacto'}</NavLink>
+    {links.map(({ link, name, spanish }) => (
+      <NavLink to={link} key={link}>
+        {isEnglish ? name : spanish}
+      </NavLink>
+    ))}
   </nav>
 )
