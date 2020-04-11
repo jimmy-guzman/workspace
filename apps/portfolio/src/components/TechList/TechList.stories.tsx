c038bfe065b19780481474e7fe4d3f7338342269
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-10 19:57:19 -0500

    refactor(portfolio): 💡 encapsulated styles for <TechList />

---
+++ apps/portfolio/src/components/TechList/TechList.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TechList } from './TechList'
+
+export default {
+  title: 'portfolio/components/TechList',
+  component: TechList
+}
+
+export const normal = () => (
+  <TechList technologies={['JavaScript', 'ReactJS', 'Redux']} />
+)
