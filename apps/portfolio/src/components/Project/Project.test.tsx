25370d879348719e38d7f7be6737e2174110a1f5
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-10 22:01:05 -0500

    refactor(portfolio): 💡 converted <Projects /> to typescript
    ✅ Closes: #33

---
+++ apps/portfolio/src/components/Project/Project.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import { Project, ProjectProps } from './Project'
+
+const mockOnProjectImageLoaded = jest.fn()
+const defaulProps = {
+  loadingProjectImage: false,
+  onProjectImageLoaded: mockOnProjectImageLoaded,
+  isEnglish: true,
+  project: {
+    name: 'name',
+    url: 'url',
+    repo: 'repo',
+    description: 'description',
+    spanish: 'spanish',
+    tech: ['react', 'typescript', 'webpack']
+  }
+}
+
+const setupProject = (props: ProjectProps = defaulProps) => {
+  return render(<Project {...props} />)
+}
+
+describe('<Project />', () => {
+  it('should render default snapshot', () => {
+    const { container } = setupProject()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+  it('should call onProjectImageLoaded when image renders', () => {
+    const { getByAltText } = setupProject()
+
+    fireEvent(getByAltText('name'), new Event('load'))
+
+    expect(mockOnProjectImageLoaded).toHaveBeenCalledTimes(1)
+  })
+  it('should contain image with no visiblity when image is not loaded', () => {
+    const { getByAltText } = setupProject({
+      ...defaulProps,
+      loadingProjectImage: true
+    })
+
+    expect(getByAltText('name')).toHaveStyle('visibility: hidden')
+  })
+})
