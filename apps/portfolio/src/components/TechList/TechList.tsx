632df0d05f9c84de132dae46e01fc2c922ad184a
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-10 19:57:19 -0500

    refactor(portfolio): 💡 encapsulated styles for <TechList />

--- apps/portfolio/src/components/TechList/TechList.tsx
+++ apps/portfolio/src/components/TechList/TechList.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import './TechList.scss'
+
 export interface TechListProps {
   technologies: string[]
 }
 
 export const TechList = ({ technologies }: TechListProps) => (
-  <ul style={{ textAlign: 'center' }}>
+  <ul
+    className='TechList'
+    style={{ textAlign: 'center', listStyle: 'none', padding: 0 }}
+  >
     {technologies.map(technology => (
       <li className='badge' key={technology}>
         {technology}
