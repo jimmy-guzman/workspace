05eb318b23cdeda2e4fe6efdb081378c71f42876
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-10 19:44:18 -0500

    refactor(portfolio): 💡 initial <Option /> to typescript

---
+++ apps/portfolio/src/components/Options/Option.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import { withMockRouter } from '../../utils'
+import { Options, OptionsProps } from './Options'
+
+const mockUpdateLang = jest.fn()
+
+const setupOptions = (props?: Pick<OptionsProps, 'isEnglish'>) => {
+  return render(
+    withMockRouter(<Options {...props} updateLang={mockUpdateLang} />)
+  )
+}
+
+describe('<Options />', () => {
+  it('should render default snapshot', () => {
+    const { container } = setupOptions()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+  it('should call updateLang with correct language', () => {
+    const { getByText } = setupOptions()
+
+    fireEvent.click(getByText('SP'))
+
+    expect(mockUpdateLang).toHaveBeenCalledWith('spanish')
+
+    fireEvent.click(getByText('ENG'))
+
+    expect(mockUpdateLang).toHaveBeenCalledWith('english')
+  })
+  it('should set correct class when spanish', () => {
+    const { getByText } = setupOptions({
+      isEnglish: false
+    })
+
+    expect(getByText('SP')).toHaveClass('active')
+  })
+  it('should set correct class when english', () => {
+    const { getByText } = setupOptions({
+      isEnglish: true
+    })
+
+    expect(getByText('ENG')).toHaveClass('active')
+  })
+})
