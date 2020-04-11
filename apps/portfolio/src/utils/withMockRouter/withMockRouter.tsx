b216e68a490c3d31835736f324d4b13e8cf458ef
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-10 19:43:30 -0500

    refactor(portfolio): 💡 simplify <Nav /> api

--- apps/portfolio/src/utils/withMockRouter/withMockRouter.tsx
+++ apps/portfolio/src/utils/withMockRouter/withMockRouter.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
 
-export const withMockRouter = (children: React.ReactNode) => {
+export const withMockRouter = (children: React.ReactNode, route?: string) => {
   const history = createMemoryHistory()
+
+  if (route) {
+    history.push(route)
+  }
 
   return (
     <Router history={history}>
