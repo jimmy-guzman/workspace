15c2c85e8591c7040d3c0a8289a2ee71198796b0
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-12 10:05:25 -0500

    refactor(portfolio): 💡 added ThemeProvider to manages colors
    ✅ Closes: #97

--- apps/portfolio/src/components/ProgressBar/ProgressBar.stories.tsx
+++ apps/portfolio/src/components/ProgressBar/ProgressBar.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ProgressBar } from './ProgressBar'
+import { withThemeProvider } from '../../utils'
 
 export default {
   title: 'portfolio/components/ProgressBar',
   component: ProgressBar,
+  decorators: [withThemeProvider],
 }
 
 export const normal = () => <ProgressBar />
--- apps/portfolio/src/components/ProgressBar/ProgressBar.tsx
+++ apps/portfolio/src/components/ProgressBar/ProgressBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { Styled } from './ProgressBar.styles'
 
 export const ProgressBar = () => (
