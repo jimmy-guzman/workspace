5488c3f1f5361723781b085b5deabf95cba617d6
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-12 08:05:15 -0500

    style(workspace): 💄 arrowParens: avoid

--- configs/webpack/utils.js
+++ configs/webpack/utils.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-const cachePath = (name) => path.join(__dirname, `../../.cache/${name}`)
+const cachePath = name => path.join(__dirname, `../../.cache/${name}`)
 const postCssConfigPath = () => path.join(__dirname, '../../postcss.config.js')
 const targetPath = () => path.resolve(process.cwd(), 'src')
 
