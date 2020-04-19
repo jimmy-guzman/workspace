3794033b711969448a4340f6e3f365bd0fafc5ed
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-19 11:16:22 -0500

    refactor(portfolio): 💡 migrated base sass to GlobalStyles

--- apps/portfolio/src/styles/global.ts
+++ apps/portfolio/src/styles/global.ts
@@ -1,11 +1,13 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 import { fontFamily, fontSize, letterSpacing } from './typography'
+import { em } from '@portfolio/utils'
 
 export const bodyStyles = css`
   font-family: ${fontFamily};
   font-size: ${fontSize};
   color: ${({ theme }) => theme.colors.secondary};
+  background: ${({ theme }) => theme.colors.tertiary};
   h1,
   h2 {
     margin: 0;
@@ -16,7 +18,30 @@ export const bodyStyles = css`
 `
 
 export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+  ::-webkit-scrollbar {
+    width: ${em('12px')};
+  }
+  ::-webkit-scrollbar-track {
+    background: inherit;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.colors.tertiary};
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: ${({ theme }) => theme.colors.secondary};
+  }
   body {
     ${bodyStyles}
   }
+  .wrapper {
+  height: 100%;
+}
 `
