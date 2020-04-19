fd822b2b124f1d86818503331382da52d8738973
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-19 17:24:11 -0500

    refactor(portfolio): 💡 `Projects` to styled component
    ✅ Closes: #119

---
+++ apps/portfolio/src/components/Project/Project.styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+import { mq, em, flex } from '@portfolio/utils'
+import { Card } from '../Card'
+import { Link } from '../Link/Link.styles'
+import { letterSpacing } from '@portfolio/styles'
+
+export const StyledProject = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  @media (min-width: ${mq('medium')}) {
+    max-width: 768px;
+    width: 80%;
+  }
+`
+
+export const StyledProjectCard = styled(Card)`
+  padding: 0 !important;
+  position: relative;
+`
+
+export const StyledProjectImage = styled.img`
+  display: block;
+  width: 100%;
+`
+
+export const StyledProjectLinks = styled.div`
+  margin-right: ${em('8px')};
+`
+
+export const StyledProjectLink = styled(Link)`
+  margin-left: ${em('16px')};
+`
+
+export const StyledProjectInfo = styled.div`
+  background: ${({ theme }) => theme.colors.tertiary};
+  padding: ${em('16px')};
+`
+export const StyledProjectDesc = styled.div`
+  margin: 0;
+`
+
+export const StyledProjectBar = styled.div`
+  background: ${({ theme }) => theme.colors.tertiary};
+  letter-spacing: ${letterSpacing.more};
+  ${flex('flex', 'row', 'space-between', 'center')}
+`
+export const StyledProjectName = styled.span`
+  padding: ${em('8px')};
+  color: ${({ theme }) => theme.colors.tertiary};
+  background: ${({ theme }) => theme.colors.primary};
+`
