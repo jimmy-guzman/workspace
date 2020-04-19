c40d446ad89b0c680f6d8d47af526e6af3f7417f
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-19 10:58:09 -0500

    refactor(portfolio): 💡 migrated body sass to GlobalStyles

---
+++ apps/portfolio/src/styles/typography.ts
@@ -0,0 +1,14 @@
+export const fontWeights = {
+  regular: 400,
+  bold: 700,
+}
+
+export const letterSpacing = {
+  less: '1px',
+  regular: '2px',
+  more: '3px',
+}
+
+export const fontFamily = "'Oswald', Impact, 'Franklin Gothic Bold', sans-serif"
+
+export const fontSize = '18px'
