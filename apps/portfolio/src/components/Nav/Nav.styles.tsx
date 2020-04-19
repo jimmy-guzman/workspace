6cdcd4481747afd6347beaa2e4518012826a2ea7
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-19 15:10:39 -0500

    refactor(portfolio): 💡 added styled flex utility

--- apps/portfolio/src/components/Nav/Nav.styles.tsx
+++ apps/portfolio/src/components/Nav/Nav.styles.tsx
@@ -1,15 +1,12 @@
 import styled from 'styled-components'
 
-import { em } from '@portfolio/utils'
+import { em, flex } from '@portfolio/utils'
 
 const Nav = styled.nav`
   background: ${({ theme }) => theme.colors.tertiary};
   width: 100%;
   height: ${em('74px')};
-  display: flex;
-  flex-flow: row;
-  justify-content: space-around;
-  align-items: center;
+  ${flex('flex', 'row', 'space-around', 'center')}
 `
 
 export const Styled = { Nav }
