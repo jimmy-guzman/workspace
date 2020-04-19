0545ba468ccb5dabe8f0eae5414a65484208ae83
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-19 14:34:23 -0500

    refactor(portfolio): 💡 aded style `<Card />` component
    ✅ Closes: #118

--- apps/portfolio/src/components/Badge/Badge.test.tsx
+++ apps/portfolio/src/components/Badge/Badge.test.tsx
@@ -16,9 +16,9 @@ describe('<Styled.Badge />', () => {
         font-family: 'Oswald',Impact,'Franklin Gothic Bold',sans-serif;
         display: inline-block;
         background: #d7d5d9;
-        border-radius: 0.16666666666666666em;
+        border-radius: 0.17em;
         color: #292b2f;
-        padding: 0.2777777777777778em;
+        padding: 0.28em;
       }
 
       <span
