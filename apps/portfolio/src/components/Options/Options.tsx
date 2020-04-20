38a9d01a8a24c44e156b7739e90f7f60915714a8
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-04-19 20:13:41 -0500

    feat(portfolio): 🎸 added dark/light theme toggle
    ✅ Closes: #116

--- apps/portfolio/src/components/Options/Options.tsx
+++ apps/portfolio/src/components/Options/Options.tsx
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom'
 
 import { Button } from '../Button'
 import { Styled } from './Options.styles'
+import { DayIcon, NightIcon } from '@portfolio/SVGs'
+import { useThemeContext, light, dark } from '@portfolio/providers'
 
 export interface OptionsProps {
   isEnglish?: boolean
@@ -14,36 +16,57 @@ export const Options = ({
   isEnglish,
   updateLang,
   activeRoute = '/',
-}: OptionsProps) => (
-  <Styled.Options>
-    <Styled.HomeLink
-      color='secondary'
-      as={({ ...props }) => (
-        <NavLink
-          exact
-          activeClassName='active-home'
-          to={activeRoute}
-          {...props}
-        >
-          Jimmy Guzman
-        </NavLink>
-      )}
-    />
-    <div>
-      <Button
-        secondary
-        active={!isEnglish}
-        onClick={() => updateLang('spanish')}
-      >
-        SP
-      </Button>
-      <Button
-        secondary
-        active={isEnglish}
-        onClick={() => updateLang('english')}
-      >
-        ENG
-      </Button>
-    </div>
-  </Styled.Options>
-)
+}: OptionsProps) => {
+  const { theme, toggleTheme } = useThemeContext()
+  return (
+    <Styled.Options>
+      <Styled.HomeLink
+        color='secondary'
+        as={({ ...props }) => (
+          <NavLink
+            exact
+            activeClassName='active-home'
+            to={activeRoute}
+            {...props}
+          >
+            Jimmy Guzman
+          </NavLink>
+        )}
+      />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div>
+          <Button
+            secondary
+            active={theme.name === light.name}
+            onClick={() => toggleTheme(light.name)}
+          >
+            <DayIcon height='20' width='20' />
+          </Button>
+          <Button
+            secondary
+            active={theme.name === dark.name}
+            onClick={() => toggleTheme(dark.name)}
+          >
+            <NightIcon height='20' width='20' />
+          </Button>
+        </div>
+        <div>
+          <Button
+            secondary
+            active={!isEnglish}
+            onClick={() => updateLang('spanish')}
+          >
+            SP
+          </Button>
+          <Button
+            secondary
+            active={isEnglish}
+            onClick={() => updateLang('english')}
+          >
+            ENG
+          </Button>
+        </div>
+      </div>
+    </Styled.Options>
+  )
+}
